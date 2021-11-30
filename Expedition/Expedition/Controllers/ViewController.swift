2f9f1d630d848b8d02194fcb94c9a89feeba80ca
Alfonso Arriaga Quezada <luisaq95nin@gmail.com>
2021-11-30 16:03:46 -0600

    Initial APP commit

---
+++ Expedition/Expedition/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Expedition
+//
+//  Created by Luis Alfonso Arriaga Quezada on 30/11/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
